c48ff374cdb7bee98381d6078829871664d79f8d
edu18RR <edu.rosas.16@gmail.com>
2023-05-09 01:09:00 -0600

    errores

--- exepciones/Fluj/src/FlujoB.java
+++ exepciones/Fluj/src/FlujoB.java
@@ -1,6 +1,8 @@
 public class FlujoB {
 
-    public static void main(String[] args) { System.out.println("Inicio del main"); metodo1();
+    public static void main(String[] args) { 
+        System.out.println("Inicio del main"); 
+        metodo1(); //main llama a metodo1
         System.out.println("Fin del main");
     }
 
@@ -8,16 +10,15 @@ public class FlujoB {
         System.out.println("Inicio del metodo1"); 
         try {
             metodo2();
-        } catch (MiException me) {
+        } catch (MiException me) { //atrapa la excepcion
             me.printStackTrace();
         }
         System.out.println("Fin del metodo1");
 
     }
 
-    private static void metodo2() {
-        //System.out.println("Inicio del metodo2");
-        //throw new MiException("Mi excepcion fue lanzada");
-        metodo2();
+    private static void metodo2() throws MiException {
+        System.out.println("Inicio del metodo2");
+        throw new MiException("Mi excepcion fue lanzada");
     }
 }
